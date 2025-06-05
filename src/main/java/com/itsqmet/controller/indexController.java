032042e2a5f824f859c9fe78558fa27e366bf923
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-06-05 17:59:26 -0500

    frond

--- src/main/java/com/itsqmet/controller/indexController.java
+++ src/main/java/com/itsqmet/controller/indexController.java
@@ -9,4 +9,52 @@ public class indexController {
     public String mostrarHome() {
         return "index";
     }
+    @GetMapping ("/gratis")
+    public String mostrarGratis() {
+        return "gratis";
+    }
+    @GetMapping ("/salonesBelleza")
+    public String mostrarSalonesBelleza() {
+        return "salonesBelleza";
+    }
+    @GetMapping ("/clinicas")
+    public String mostrarClinicas() {
+        return "clinicas";
+    }
+    @GetMapping ("/consultorias")
+    public String mostrarConsultorias() {
+        return "consultorias";
+    }
+    @GetMapping ("/educacion")
+    public String mostrarEducacion() {
+        return "educacion";
+    }
+    @GetMapping ("/talleres")
+    public String mostrarTalleres() {
+        return "talleres";
+    }
+    @GetMapping ("/demo")
+    public String mostrarDemo() {
+        return "demo";
+    }
+    @GetMapping ("/gestionPersonal")
+    public String mostrarGestionPersonal() {
+        return "gestionPersonal";
+    }
+    @GetMapping ("/agendamiento")
+    public String mostrarAgendamiento() {
+        return "agendamiento";
+    }
+    @GetMapping ("/control")
+    public String mostrarControl() {
+        return "control";
+    }
+    @GetMapping ("/precios")
+    public String mostrarPrecios() {
+        return "precios";
+    }
+    @GetMapping ("/agradecimiento")
+    public String mostrarAgradecimiento() {
+        return "agradecimiento";
+    }
 }

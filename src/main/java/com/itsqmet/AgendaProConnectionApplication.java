a266d5c73797140f2b099aacf9503572c345498a
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-06-04 23:00:43 -0500

    index falta img

--- src/main/java/com/itsqmet/AgendaProConnectionApplication.java
+++ src/main/java/com/itsqmet/AgendaProConnectionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgendaProConnectionApplication {
+public class  AgendaProConnectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaProConnectionApplication.class, args);
